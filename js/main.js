205d040eabd56370e23e6be2d2290e819e48aedc
MichalRotkop <shalvi.r@gmail.com>
2024-01-31 19:41:54 +0200

    Add getRandomColor to util. ball color changes on click

--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@ var gBallSize = 100
 function onBallClick(elBall) {
     gBallSize = (gBallSize >= 400)? 100 : gBallSize + getRandomInt(20,61)
     document.querySelector(':root').style.setProperty('--ball-size', `${gBallSize}px`)
+    elBall.style.backgroundColor = getRandomColor()
     elBall.innerHTML = `${gBallSize}`
 }
