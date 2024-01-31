825323bd3d6c62fc0ce277b804f25cbf5aed685f
MichalRotkop <shalvi.r@gmail.com>
2024-01-31 20:06:44 +0200

    Add a second ball with a different initial color

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var gBallSize = 100
 
 function onBallClick(elBall) {
     gBallSize = (gBallSize >= 400)? 100 : gBallSize + getRandomInt(20,61)
-    document.querySelector(':root').style.setProperty('--ball-size', `${gBallSize}px`)
+    elBall.style.width = `${gBallSize}px`
+    elBall.style.height = `${gBallSize}px`
     elBall.style.backgroundColor = getRandomColor()
     elBall.innerHTML = `${gBallSize}`
 }
